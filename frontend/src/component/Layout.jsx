85e5500452421ca8ca422dda612320b187267353
saki2024 <sakiasikomaidrakulu@gmail.com>
2024-07-19 18:08:02 +1200

    added search bar

--- frontend/src/component/Layout.jsx
+++ frontend/src/component/Layout.jsx
@@ -21,6 +21,50 @@ import MailIcon from "@mui/icons-material/Mail";
 import MovieGrid from "./MovieGrid";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import cinemaLogo from "../images/lOGO.png";
+import SearchIcon from "@mui/icons-material/Search";
+import { alpha, InputBase } from "@mui/material";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
 
 const darkTheme = createTheme({
   palette: {
@@ -165,6 +209,15 @@ export default function Layout() {
                 </ListItem>
               ))}
             </List>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
           </Toolbar>
         </AppBar>
         <Drawer variant="permanent" open={open}>
